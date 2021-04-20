4d945e5237a723a96ed41fb19c4bd88865457ca6
226wyj <308033240@qq.com>
2021-04-20 13:16:28 +0800

    2021/4/20-13:16 add bootstrap4

--- app/app.py
+++ app/app.py
@@ -1,14 +1,26 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask import render_template
+# from flask_moment import Moment
+from flask_wtf import Form
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+# moment = Moment(app)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/static/js/paint.js
+++ app/static/js/paint.js
@@ -1,72 +1,74 @@
 var canvas;
 var context;
 
-// 初始化
+// Initialize
 window.onload = function() {
-  // 获取画布已经绘图上下文
-  canvas = document.getElementById("drawingCanvas");
-  context = canvas.getContext("2d");
+	// Get canvas and the picture's context.
+	canvas = document.getElementById("drawingCanvas");
+	context = canvas.getContext("2d");
 
-  // 画布添加鼠标事件
-  canvas.onmousedown = startDrawing;
-  canvas.onmouseup = stopDrawing;
-  canvas.onmouseout = stopDrawing;
-  canvas.onmousemove = draw;
+	// Add mouse action listener to canvas.
+	canvas.onmousedown = startDrawing;
+	canvas.onmouseup = stopDrawing;
+	canvas.onmouseout = stopDrawing;
+	canvas.onmousemove = draw;
 };
 
-// 记录当前是否在画图
+// Judge whether is drawing.
 var isDrawing = false;
 
-// 开始画图
+// Start drawing.
 function startDrawing(e) {
-  isDrawing = true;
-  // 创建一个新的绘图路径
-  context.beginPath();
-  // 把画笔移动到鼠标位置
-  context.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+	isDrawing = true;
+	// 创建一个新的绘图路径
+	context.beginPath();
+	// 把画笔移动到鼠标位置
+	context.moveTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
 }
 
-// 停止画图
+// Stop drawing.
 function stopDrawing() {
-  isDrawing = false;
+  	isDrawing = false;
 }
 
-//画图中
+// 画图中
 function draw(e) {
   if (isDrawing == true) {
-    // 找到鼠标最新位置
-    var x = e.pageX - canvas.offsetLeft;
-    var y = e.pageY - canvas.offsetTop;
-    // 画一条直线到鼠标最新位置
-    context.lineTo(x, y);
-    context.stroke();  
+	// 找到鼠标最新位置
+	var x = e.pageX - canvas.offsetLeft;
+	var y = e.pageY - canvas.offsetTop;
+	// 画一条直线到鼠标最新位置
+	context.lineTo(x, y);
+	context.stroke();  
   }
 }
 
-// 保存之前选择的颜色的画笔 <img> 元素标签
+// 保存之前选择的颜色的画笔 <button> 元素标签
 var previousColorElement;
 
 // 改变画笔颜色
-function changeColor(color, imgElement) {    
-  context.strokeStyle = color;
-  // 将当前画笔的 <img> 元素标签设置为选中样式
-  imgElement.className = "Selected";
-  // 将之前的画笔的 <img> 元素标签恢复默认样式
-  if (previousColorElement != null) previousColorElement.className = "";
-  previousColorElement = imgElement;
+function changeColor(color, btnElement) {    
+	context.strokeStyle = color;
+	// 将当前画笔的 <button> 元素标签设置为选中样式
+	btnElement.className = "btn btn-primary active";
+	// 将之前的画笔的 <button> 元素标签恢复默认样式
+	if (previousColorElement != null) 
+		previousColorElement.className = "btn btn-default";
+	previousColorElement = btnElement;
 }
 
-// 保存之前选择的粗细的画笔 <img> 元素标签
+// 保存之前选择的粗细的画笔 <button> 元素标签
 var previousThicknessElement;
 
 // 改变画笔粗细
-function changeThickness(thickness, imgElement) {    
-  context.lineWidth = thickness;
-  // 将当前画笔的 <img> 元素标签设置为选中样式
-  imgElement.className = "Selected";
-  // 将之前的画笔的 <img> 元素标签恢复默认样式
-  if (previousThicknessElement != null) previousThicknessElement.className = "";
-  previousThicknessElement = imgElement;
+function changeThickness(thickness, btnElement) {    
+	context.lineWidth = thickness;
+	// 将当前画笔的 <button> 元素标签设置为选中样式
+	btnElement.className = "btn btn-primary active";
+	// 将之前的画笔的 <button> 元素标签恢复默认样式
+	if (previousThicknessElement != null)
+		previousThicknessElement.className = "btn btn-default";
+	previousThicknessElement = btnElement;
 }
 
 // 清除画布
